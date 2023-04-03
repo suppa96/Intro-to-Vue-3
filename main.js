6436e68050c046f08914bd4fec9aef6fbf7e3338
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2023-04-03 12:41:33 +0200

    complete lesson

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_green.jpg",
+      url: "https://www.youtube.com/watch?v=xvFZjo5PgG0",
+    };
+  },
+  // versione abbreviata di data: function () {}
+});
