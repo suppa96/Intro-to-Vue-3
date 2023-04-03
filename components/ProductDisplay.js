a8d27ba2f250824597b9016a1c80d1190e249395
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2023-04-03 17:53:28 +0200

    complete exercise

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,13 +1,13 @@
-app.component('product-display', {
+app.component("product-display", {
   props: {
     premium: {
       type: Boolean,
-      required: true
-    }
+      required: true,
+    },
   },
-  template: 
-  /*html*/
-  `<div class="product-display">
+  template:
+    /*html*/
+    `<div class="product-display">
     <div class="product-container">
       <div class="product-image">
         <img v-bind:src="image">
@@ -44,39 +44,53 @@ app.component('product-display', {
   </div>`,
   data() {
     return {
-        product: 'Socks',
-        brand: 'Vue Mastery',
-        selectedVariant: 0,
-        details: ['50% cotton', '30% wool', '20% polyester'],
-        variants: [
-          { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-        ]
-    }
+      product: "Socks",
+      brand: "Vue Mastery",
+      selectedVariant: 0,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
+      ],
+    };
   },
   methods: {
-      addToCart() {
-          this.cart += 1
-      },
-      updateVariant(index) {
-          this.selectedVariant = index
-      }
+    addToCart() {
+      //this.cart += 1
+      //Ogni volta che viene premuto il bottone mando un'info al main, emettendo un evento
+      this.$emit("add-to-cart", this.variants[this.selectedVariant].id);
+      // per inviare anche l'id, passo un secondo parametro all'event emitter
+      // l'id della variante selezionata
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
   },
   computed: {
-      title() {
-          return this.brand + ' ' + this.product
-      },
-      image() {
-          return this.variants[this.selectedVariant].image
-      },
-      inStock() {
-          return this.variants[this.selectedVariant].quantity
-      },
-      shipping() {
-        if (this.premium) {
-          return 'Free'
-        }
-        return 2.99
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free";
       }
-  }
-})
+      return 2.99;
+    },
+  },
+});
--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            premium: true
-        }
+  data() {
+    return {
+      cart: [],
+      // provo a cambiare il cart da un numero ad un array
+      premium: true,
+    };
+  },
+  methods: {
+    //cambio il metodo per far sì che aggiunga id ad un array anziché
+    // aumentare un semplice contatore
+    updateCart(id) {
+      this.cart.push(id);
     },
-    methods: {}
-})
+  },
+});
