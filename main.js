d9fb2c5575b7f50df0d8c16012e1fcfc91424712
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2023-04-03 12:57:35 +0200

    complete exercise

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: true,
+      inventory: 10,
+    };
+  },
+});
